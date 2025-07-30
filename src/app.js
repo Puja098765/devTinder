076de1b9211f25f021d797b19c1bb57daf19df53
Puja <pujakeshri098765@gmail.com>
2025-07-30 19:10:31 +0530

    Schema validation

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const User = require("./models/user");
 
 // Middleware activated for all the routes,it reads the json in postman convert into js obj and add js object back to req
 app.use(express.json());
+
 // sending data to database
 app.post("/signup",async (req,res)=>{
     // Creating a new instance of the User model
@@ -18,6 +19,57 @@ app.post("/signup",async (req,res)=>{
         res.status(400).send("Error saving the user:" + err.message);
     }
 });
+
+// Get user by email
+app.get("/user",async (req,res)=>{
+  const userEmail = req.body.emailId;
+
+  try{
+    const users = await User.find({emailId: userEmail});
+    if(users.length === 0){
+          res.status(404).send("User not found");
+    } else {
+        res.send(users);
+    }
+  
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Feed API - GET /feed -get all the users from the database
+app.get("/feed", async (req,res)=>{
+    try{
+        const users =  await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status.send("Something went wrong");
+    }
+});
+// Delete a user from the database
+app.delete("/user", async (req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted Successfully");
+    } catch (err){
+        res.status(400).send("Something went wrong");
+    }
+});
+// Update data of the user
+app.patch("/user", async (req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        await User.findByIdAndUpdate({_id: userId }, data, {
+            runValidators: true,
+        });
+        res.send("User updated sucessfully");
+    } catch (err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
 connectDB()
 .then(()=>{
     console.log("Database connection established");
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,52 @@ const mongoose = require("mongoose");
 // Defining User Schema
 const userSchema = new mongoose.Schema({
 firstName:{
-    type:String
+    type:String,
+    required: true,
+    minLength:4,
+    maxLength:50,
 },
 lastName:{
-    type:String
+    type:String,
+    
 },
 emailId:{
-    type:String
+    type:String,
+    required: true,
+    unique:true,
+    lowercase:true,
+    trim: true,
 },
 password:{
-    type:String
+    type:String,
+    required: true,
 },
 age:{
-    type:Number
+    type:Number,
+    min:18,
+    
 },
 gender:{
-    type:String
-}
+    type:String,
+    validate(value){
+        if (!["male","female","others"].includes(value)){
+            throw new Error("Gender data is not valid");
+        }
+    }
+},
+photoUrl:{
+    type: String,
+    default:"https://toppng.b-cdn.net/uploads/preview/donna-picarro-dummy-avatar-115633298255iautrofxa.png",
+},
+about:{
+    type: String,
+    default: "This is a default about of the user!", 
+},
+skills:{
+    type:[String],
+},
+},{
+    timestamps: true,
 });
 // create mongoose models 1st parameter = name of model,2nd= useSchema
  module.exports = mongoose.model("User",userSchema);
