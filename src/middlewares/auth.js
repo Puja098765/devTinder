2a38302c22271ef2fb9f447c4a95cd6e2c1d17a7
Puja <pujakeshri098765@gmail.com>
2025-08-02 09:53:04 +0530

    using express-Router

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,21 +1,28 @@
 // Handle Auth Middleware for all GET POST,... requests
-// this middleware only be called for /admin
- const adminAuth= (req,res,next)=>{
-    const token ="xyz";
-    const isAdminAuthorized= token === "xyz";
-    if(!isAdminAuthorized){
-        res.status(401).send("Unauthorized request");
-    } else {
-        next();
+// this middleware only be called for /user
+const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+
+ const userAuth= async (req,res,next)=>{
+//  Read the token from the req cookies
+try {
+    const {token} = req.cookies;
+    if (!token) {
+        throw new Error("Token is not valid!!!!!");
     }
+    //Validate my token
+const decodedObj = await jwt.verify(token,"DEV@Tinder$790");
+const {_id} = decodedObj;
+const user = await User.findById(_id);
+if (!user){
+    throw new Error("User not found");
+}
+req.user =user;
+next();
+ } catch (err) {
+ res.status(400).send("ERROR: " +err.message);
+ }
+// Validate the token
+// Find the user
 };
- const userAuth= (req,res,next)=>{
-    const token ="xyzabc";
-    const isAdminAuthorized= token === "xyz";
-    if(!isAdminAuthorized){
-        res.status(401).send("Unauthorized request");
-    } else {
-        next();
-    }
-};
-module.exports={adminAuth,userAuth};
+module.exports={userAuth};
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Defining User Schema
 const userSchema = new mongoose.Schema({
@@ -66,5 +68,18 @@ skills:{
 },{
     timestamps: true,
 });
+userSchema.methods.getJWT = async function () {
+    const user = this;
+    const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
+        expiresIn: "7d",
+    });
+    return token;
+};
+userSchema.methods.validatePassword = async function (passwordInputByUser){
+    const user = this;
+    const passwordHash = user.password;
+    const isPasswordValid = await bcrypt.compare(passwordInputByUser,passwordHash);
+    return isPasswordValid;
+}
 // create mongoose models 1st parameter = name of model,2nd= useSchema
  module.exports = mongoose.model("User",userSchema);
