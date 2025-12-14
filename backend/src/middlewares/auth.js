f175f57ec068a39d7e3a383af71e82ff8d01553a
Puja Keshri <pujakeshri098765@gmail.com>
2025-12-14 15:26:09 +0530

    redirect to login if not authenticated

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req,res,next) =>{
 //    Read the token from the req cookies 
 const {token} = req.cookies;
 if(!token){
-    throw new Error("No token found");
+    return res.status(401).send("Please login!");
 }
  // Validate the token
 const decodeObj = await jwt.verify(token, "DEV@Tinder$790");
