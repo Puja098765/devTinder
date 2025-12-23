49418a669a63d3ef20ba0c9132a9417b255abe00
Puja <114655857+Puja098765@users.noreply.github.com>
2025-12-24 00:15:42 +0530

    Update BASE_URL for backend service

--- frontend/devTinder/src/utils/constants.js
+++ frontend/devTinder/src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL ="https://devtinder-backend-m9al.onrender.com" || "http://localhost:3000";
+// export const BASE_URL ="https://devtinder-backend-m9al.onrender.com" || "http://localhost:3000";
+export const BASE_URL ="https://devtinder-backnd.onrender.com" || "http://localhost:3000";
