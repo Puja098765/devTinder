d8d9e114a9172ba043b1113ee4e77ee844584f3a
Puja Keshri <pujakeshri098765@gmail.com>
2025-12-23 18:12:53 +0530

    changes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -32,6 +32,9 @@ authRouter.post("/signup",  async (req,res)=>{
   const token = await savedUser.getJWT();
 
   res.cookie("token", token, {
+    httpOnly: true,
+     secure:true,
+    sameSite:"none",
   expires: new Date(Date.now() + 8 * 3600000), // 1 day
   });
    res.json({ message: "User Added succcessfully!", data: savedUser});
